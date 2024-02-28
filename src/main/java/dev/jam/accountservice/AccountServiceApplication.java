9a92928bb6cfb8da6b20b0dfaa538da2b7fbad0a
Haitam Elgharras <elgharras.haitam@gmail.com>
2024-02-28 23:29:38 +0100

    Cors config

--- src/main/java/dev/jam/accountservice/AccountServiceApplication.java
+++ src/main/java/dev/jam/accountservice/AccountServiceApplication.java
@@ -13,10 +13,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableConfigurationProperties(JwtConfig.class)
 @EnableFeignClients
+@CrossOrigin
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
